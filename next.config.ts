b1f7407c716497f7aae91ec115f173b5af96dcce
Arjav Patel <arjav1528@gmail.com>
2025-08-03 12:46:05 +0530

    made some changes for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'], // For Google profile images
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['formidable'],
+  },
 };
 
 export default nextConfig;
